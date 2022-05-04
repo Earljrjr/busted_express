cb244d90202cda4f3c265df031f72257bedda2e7
Mike <rhodes_michael87@yahoo.com>
2022-05-04 11:10:50 -0400

    3 errors

--- app.js
+++ app.js
@@ -4,17 +4,17 @@ const hostname = '127.0.0.1';
 const port = 3333;
 
 const express = require('express'),
-    es6Renderer = require('express-es6-template-engine');
+    es6Renderer = require('express-es6-template-engine'); // missing const for a variable 
 
 const app = express();
 
-app.engine('html', es6Renderer);
+app.engine('html', es6Renderer);  // undefined due to no variable
 app.set('views', './views');
 app.set('view engine', 'html');
 
 const server = http.createServer(app);
 
-server.listen(PORT, HOSTNAME, () => {
+server.listen(PORT, HOSTNAME, () => {       // needs to be lowercase 
     console.log(`Server running at http://${hostname}:${port}`)
 });
 
